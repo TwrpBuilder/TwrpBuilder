fc10545b6c25051b4f22921d7a26b65da7a0a4c5
Grace5921 <androidlover5842@gmail.com>
2016-11-13 16:53:05 +0530

    get recovery path on click

--- app/src/main/java/github/grace5921/TwrpBuilder/Fragment/BackupFragment.java
+++ app/src/main/java/github/grace5921/TwrpBuilder/Fragment/BackupFragment.java
@@ -7,10 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.List;
+
+import eu.chainfire.libsuperuser.Shell;
 import github.grace5921.TwrpBuilder.R;
 import github.grace5921.TwrpBuilder.app.Activity;
 import github.grace5921.TwrpBuilder.config.Config;
+import github.grace5921.TwrpBuilder.util.ShellExecuter;
 import github.grace5921.TwrpBuilder.util.ShellUtils;
 
 /**
@@ -21,6 +26,7 @@ public class BackupFragment extends Fragment
 {
     ShellUtils mShell;
     private Button mBackupButton;
+    private TextView ShowOutput;
 
     @Nullable
     @Override
@@ -30,6 +36,7 @@ public class BackupFragment extends Fragment
         View view = inflater.inflate(R.layout.fragment_backup, container, false);
         /*this.mShell = ((Activity) getActivity()).getShellSession();*/
         mBackupButton=(Button)view.findViewById(R.id.BackupButton);
+        ShowOutput=(TextView)view.findViewById(R.id.show_output);
 
         if(Config.checkBackup()) {
             mBackupButton.setEnabled(false);
@@ -40,12 +47,19 @@ public class BackupFragment extends Fragment
                     @Override
                     public void onClick(View v) {
                         mBackupButton.setEnabled(false);
+                        ShowOutput.setText(recovery_output);
                     }
                 }
         );
 
         return view;
     }
+
+    private static final List<String> BackupTwrp= Shell.SU.run("ls -la `find /dev/block/platform/ -type d -name \"by-name\"` | grep RECOVERY");
+    private String store_dev_output=String.valueOf(BackupTwrp);
+    private String[] parts = store_dev_output.split("\\s+");
+    private String[] recovery_output_last = parts[7].split("\\]");
+    private String recovery_output=recovery_output_last[0];
 
     @Override
     public void onResume() {
