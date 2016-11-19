a68f48db9b6af58ebe8aae41d196c9925cb9720d
Grace5921 <androidlover5842@gmail.com>
2016-11-19 12:59:36 +0530

    typos

--- app/src/main/java/github/grace5921/TwrpBuilder/MainActivity.java
+++ app/src/main/java/github/grace5921/TwrpBuilder/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
