b88e19dbbd6eb87e794c5868175e3d62ccdeba44
Ethan Yonker <dees_troy@teamw.in>
2016-12-05 22:22:24 -0500

    Update github api link
    Change-Id: I68b41a75cf0d1fa57b693c726cccd382a16f91b1

--- app/src/main/java/github/grace5921/TwrpBuilder/util/Config.java
+++ app/src/main/java/github/grace5921/TwrpBuilder/util/Config.java
@@ -11,6 +11,6 @@ public class Config
    public static boolean suAvailable(){return new File("/system/xbin/su").isFile();}
    public static boolean checkBackup(){return new File("/sdcard/TwrpBuilder/TwrpBuilderRecoveryBackup.tar").isFile();}
    public static boolean CheckDownloadedTwrp(){return new File("/sdcard/TwrpBuilder/Twrp.img").isFile();}
-   public final static String URL_CONTRIBUTORS = "https://api.github.com/repos/grace5921/twrpBuilder/contributors";
-   public final static String URL_APP_RELEASES ="https://api.github.com/repos/grace5921/twrpBuilder/releases";
+   public final static String URL_CONTRIBUTORS = "https://api.github.com/repos/TwrpBuilder/TwrpBuilder/contributors";
+   public final static String URL_APP_RELEASES ="https://api.github.com/repos/TwrpBuilder/TwrpBuilder/releases";
 }
