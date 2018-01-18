304f62d904e5394a50d7b0182fe42774ede25dd8
androidlover5842 <androidlover5842@gmail.com>
2018-01-18 17:06:45 +0530

    devFragment: allow developers to fetch userr files

--- app/src/main/java/github/grace5921/TwrpBuilder/Fragment/DevsFragment.java
+++ app/src/main/java/github/grace5921/TwrpBuilder/Fragment/DevsFragment.java
@@ -1,7 +1,12 @@
 package github.grace5921.TwrpBuilder.Fragment;
 
+import android.app.DownloadManager;
 import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,18 +14,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseListOptions;
 import com.firebase.ui.database.SnapshotParser;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -36,6 +47,10 @@ public class DevsFragment extends Fragment {
 
     private Context context;
     private FirebaseListAdapter<User> adapter;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private ListView  mListView;
+    private Query query;
 
     public DevsFragment(Context context)
     {
@@ -44,12 +59,13 @@ public class DevsFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_devs, container, false);
-        final ListView  mListView = (ListView) view.findViewById(R.id.Lv_devs);
+        storage = FirebaseStorage.getInstance();
+        storageRef=storage.getReference();
+        mListView = (ListView) view.findViewById(R.id.Lv_devs);
 
-
-        Query query = FirebaseDatabase.getInstance()
+        query = FirebaseDatabase.getInstance()
                 .getReference("Uploader");
 
         Log.i("Deva",query.toString());
@@ -62,17 +78,46 @@ public class DevsFragment extends Fragment {
 
         adapter = new FirebaseListAdapter<User>(options) {
             @Override
-            protected void populateView(View v, User model, int position) {
+            protected void populateView(View v, final User model, int position) {
                 TextView tvEmail = v.findViewById(R.id.list_user_email);
                 TextView tvDevice = v.findViewById(R.id.list_user_device);
                 TextView tvBoard = v.findViewById(R.id.list_user_board);
                 TextView tvDate= v.findViewById(R.id.list_user_date);
                 TextView tvBrand = v.findViewById(R.id.list_user_brand);
-                tvDate.setText(model.WtDate());
-                tvEmail.setText(model.WEmail());
-                tvDevice.setText(model.WModel());
-                tvBoard.setText(model.WBoard());
-                tvBrand.setText(model.WBrand());
+                Button btFiles=v.findViewById(R.id.BtFile);
+                tvDate.setText("Date : "+model.WtDate());
+                tvEmail.setText("Email : "+model.WEmail());
+                tvDevice.setText("Model : " + model.WModel());
+                tvBoard.setText("Board : "+model.WBoard());
+                tvBrand.setText("Brand : " +model.WBrand());
+
+                btFiles.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view1) {
+                        storageRef.child("queue/" + model.WBrand() + "/" + model.WBoard() + "/" + model.WModel() + "/TwrpBuilderRecoveryBackup.tar" ).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Log.i("Deva","yah");
+                                DownloadManager downloadManager = (DownloadManager) context.getSystemService(getContext().DOWNLOAD_SERVICE);
+
+                                DownloadManager.Request request = new DownloadManager.Request(uri);
+                                String fileName=model.WModel()+"-"+model.WBoard()+"-"+model.WEmail()+".tar";
+                                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                Long reference = downloadManager.enqueue(request);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle any errors
+                            }
+                        });
+
+                        Toast.makeText(context,model.WModel(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+
             }
         };
         mListView.setAdapter(adapter);
@@ -87,7 +132,8 @@ public class DevsFragment extends Fragment {
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
