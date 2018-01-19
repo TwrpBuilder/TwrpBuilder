373509f2286657d841a447fc6afd1ee6d5e3264c
androidlover5842 <androidlover5842@gmail.com>
2018-01-19 21:03:45 +0530

    switch to viewpager and add builds done fragment
    unstable for now

---
+++ app/src/main/java/github/grace5921/TwrpBuilder/Fragment/DevsBuildDoneFragment.java
@@ -0,0 +1,160 @@
+package github.grace5921.TwrpBuilder.Fragment;
+
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import github.grace5921.TwrpBuilder.R;
+import github.grace5921.TwrpBuilder.util.User;
+
+/**
+ * Created by androidlover5842 on 19/1/18.
+ */
+
+public class DevsBuildDoneFragment extends Fragment {
+
+    private FirebaseListAdapter<User> adapter;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private ListView mListView;
+    private Query query;
+    private DatabaseReference mUploader;
+    private FirebaseDatabase mFirebaseInstance;
+    private String userId;
+
+    DevsBuildDoneFragment(){
+        //Empty Constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_dev_build_done, container, false);
+        storage = FirebaseStorage.getInstance();
+        storageRef=storage.getReference();
+        mListView = (ListView) view.findViewById(R.id.lv_build_done);
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mUploader = mFirebaseInstance.getReference("Builds");
+        userId = mUploader.push().getKey();
+        query = FirebaseDatabase.getInstance()
+                .getReference("RunningBuild");
+
+        FirebaseListOptions<User> options = new FirebaseListOptions.Builder<User>()
+                .setLayout(R.layout.list_developer_stuff)
+                .setQuery(query,User.class)
+                .build();
+
+        adapter = new FirebaseListAdapter<User>(options) {
+            @Override
+            protected void populateView(View v, final User model, int position) {
+                TextView tvEmail = v.findViewById(R.id.list_user_email);
+                TextView tvDevice = v.findViewById(R.id.list_user_device);
+                TextView tvBoard = v.findViewById(R.id.list_user_board);
+                TextView tvDate= v.findViewById(R.id.list_user_date);
+                TextView tvBrand = v.findViewById(R.id.list_user_brand);
+                Button btFiles=v.findViewById(R.id.BtFile);
+                final Button btStartBuild=v.findViewById(R.id.bt_start_build);
+                final Button btBuildDone=v.findViewById(R.id.bt_build_done);
+                tvDate.setText("Date : "+model.WtDate());
+                tvEmail.setText("Email : "+model.WEmail());
+                tvDevice.setText("Model : " + model.WModel());
+                tvBoard.setText("Board : "+model.WBoard());
+                tvBrand.setText("Brand : " +model.WBrand());
+                btStartBuild.setVisibility(View.GONE);
+                btBuildDone.setVisibility(View.VISIBLE);
+
+                btFiles.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view1) {
+                        storageRef.child("queue/" + model.WBrand() + "/" + model.WBoard() + "/" + model.WModel() + "/TwrpBuilderRecoveryBackup.tar" ).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                DownloadManager downloadManager = (DownloadManager) getContext().getSystemService(getContext().DOWNLOAD_SERVICE);
+
+                                DownloadManager.Request request = new DownloadManager.Request(uri);
+                                String fileName=model.WModel()+"-"+model.WBoard()+"-"+model.WEmail()+".tar";
+                                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                Long reference = downloadManager.enqueue(request);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                Toast.makeText(getContext(),"Failed",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+                        Toast.makeText(getContext(),model.WModel(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                /*btStartBuild.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        btBuildDone.setVisibility(View.VISIBLE);
+                        btStartBuild.setVisibility(View.GONE);
+                        mFirebaseInstance.getReference("RunningBuild").addListenerForSingleValueEvent(
+                                new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        for (DataSnapshot child: dataSnapshot.getChildren()) {
+                                            child.getRef().removeValue();
+                                        }
+                                    }
+
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+                                        Log.w("TodoApp", "getUser:onCancelled", databaseError.toException());
+                                    }
+                                });
+
+                        User user = new User(model.WBrand(),model.WBoard(),model.WModel(),model.WEmail(),model.WUid(),model.WFmcToken(),model.WtDate());
+                        mUploader.child(userId).setValue(user);
+                        System.out.println(model.WBrand()+model.WBoard()+model.WModel()+model.WEmail()+model.WtDate());
+                    }
+                });*/
+
+
+            }
+        };
+        mListView.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
--- app/src/main/java/github/grace5921/TwrpBuilder/Fragment/DevsFragment.java
+++ app/src/main/java/github/grace5921/TwrpBuilder/Fragment/DevsFragment.java
@@ -9,8 +9,12 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,6 +41,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import github.grace5921.TwrpBuilder.R;
 import github.grace5921.TwrpBuilder.app.Activity;
@@ -52,28 +57,57 @@ import static github.grace5921.TwrpBuilder.firebase.FirebaseInstanceIDService.re
 public class DevsFragment extends Fragment {
 
     private Context context;
-    private FragmentTabHost mTabHost;
-
-    public DevsFragment(Context context)
-    {
-        this.context=context;
+    public DevsFragment(Context context) {
+        this.context = context;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_devs, container, false);
-        mTabHost = (FragmentTabHost) view.findViewById(android.R.id.tabhost);
-        mTabHost.setup(getActivity(), getChildFragmentManager(),
-                R.id.tabcontent);
 
-        mTabHost.addTab(mTabHost.newTabSpec("Building").setIndicator("Building"),
-                DevsBuildRunningFragment.class, null);
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new DevsBuildRunningFragment(), "Builds Running");
+        adapter.addFragment(new DevsInQueueFragment(), "Builds In Queue");
+        adapter.addFragment(new DevsBuildDoneFragment(),"Builds Completed");
+        viewPager.setAdapter(adapter);
 
-        mTabHost.addTab(mTabHost.newTabSpec("In Queue").setIndicator("In Queue"),
-                DevsInQueueFragment.class, null);
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
         return view;
     }
 
+    public static class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+
+
+    }
 }
