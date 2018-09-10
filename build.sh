c5bb24da8c1e1618d8190fa13ed6d028a539e40b
Dyneteve <sound0020@gmail.com>
2018-09-10 20:10:44 +0300

    Fix travis build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 curl -H 'PRIVATE-TOKEN: $token' 'https://gitlab.com/api/v4/projects/8312758/repository/files/twrpbuilderkey.jks/raw?ref=master' -o twrpbuilderkey.jks
 curl -H 'PRIVATE-TOKEN: $token' 'https://gitlab.com/api/v4/projects/8312758/repository/files/keystore.properties/raw?ref=master' -o keystore.properties
+chmod 777 twrpbuilderkey.jks
+chmod 777 keystore.properties
 mv twrpbuilderkey.jks app/
